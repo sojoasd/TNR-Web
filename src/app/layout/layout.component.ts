33bb6b3bd5739ea87cbc4c9b6382de4f16aa443b
zeal yen <zealyen@noodoe.com>
2020-04-10 07:51:29 +0800

    layout add logout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  selector: "app-layout",
+  templateUrl: "./layout.component.html",
+  styleUrls: ["./layout.component.css"]
 })
 export class LayoutComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  logout() {
+    localStorage.removeItem("tnr-user");
+    this.router.navigate(["/login"]);
   }
-
 }
